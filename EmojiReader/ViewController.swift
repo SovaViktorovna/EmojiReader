67240aac864e34ba7ee57087d121f1c64911e5de
Viktoria Demchenko <sovaviktorovna430@gmail.com>
2024-08-29 10:37:04 +0400

    Created new project

---
+++ EmojiReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiReader
+//
+//  Created by Виктория Демченко on 29.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
